df9871f3d6ec6dea917a130c732f620cad47e920
mister-josuecedeno <josuecedeno@yahoo.com>
2022-08-03 18:20:04 -0500

    Add project files.

---
+++ Edabit/Very_Easy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit
+{
+    public static class Very_Easy
+    {
+        public static int area(int h, int w)
+        {
+            if (h <= 0 || w <= 0) return -1;
+            return h * w;
+        }
+    }
+}
---
+++ Edabit_Very_Easy_Test/Very_Easy_Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Edabit;
+
+namespace Edabit.Tests
+{
+    public class Very_Easy_Tests
+    {
+        [Theory]
+        [InlineData(5, 3, 15)]
+        [InlineData(8, 5, 40)]
+        [InlineData(5, 4, 20)]
+        [InlineData(2, 3, 6)]
+        [InlineData(10000, 10000, 100000000)]
+        [InlineData(-2, -5, -1)]
+        [InlineData(0, 3, -1)]
+        [InlineData(5, -3, -1)]
+        [InlineData(0, 1, -1)]
+        [InlineData(-1, 0, -1)]
+        public void Area_SimpleValuesShouldCalculate(int h, int w, int expected)
+        {
+            // Arrange
+
+            // Act
+            double actual = Very_Easy.area(h, w);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
