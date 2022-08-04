3f760da2ace1969ed5e647b575b3a491514d5c56
mister-josuecedeno <josuecedeno@yahoo.com>
2022-08-03 19:13:12 -0500

    unit test practice

--- Edabit/Very_Easy.cs
+++ Edabit/Very_Easy.cs
@@ -8,6 +8,21 @@ namespace Edabit
 {
     public static class Very_Easy
     {
+        public static int absolute(int num)
+        {
+            return num < 0 ? -num : num;
+        }
+
+        public static bool IsOdd(int num)
+        {
+            return num % 2 != 0;
+        }
+
+        public static bool Comp(string str1, string str2)
+        {
+            return str1.Length == str2.Length;
+        }
+
         public static int ReturnNegative(int n)
         {
             return n < 0 ? n : n * -1;
--- Edabit_Very_Easy_Test/Very_Easy_Tests.cs
+++ Edabit_Very_Easy_Test/Very_Easy_Tests.cs
@@ -11,6 +11,64 @@ namespace Edabit.Tests
     public class Very_Easy_Tests
     {
         [Theory]
+        [InlineData(-5, 5)]
+        [InlineData(-3, 3)]
+        [InlineData(250, 250)]
+        [InlineData(0, 0)]
+        [InlineData(6, 6)]
+        [InlineData(1103, 1103)]
+        public void Absolute_ReturnAnAbsoluteValueOfAValidNumber(int num, int expected)
+        {
+            // Arrange
+
+            // Act
+            int actual = Very_Easy.absolute(num);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+        
+        [Theory]
+        [InlineData(-5, true)]
+        [InlineData(-50, false)]
+        [InlineData(0, false)]
+        [InlineData(25, true)]
+        [InlineData(-19, true)]
+        [InlineData(12, false)]
+        [InlineData(23, true)]
+        [InlineData(-71, true)]
+        public void IsOdd_ReturnTrueIfNumberIsOdd (int num, bool expected)
+        {
+            // Arrange
+
+            // Act
+            bool actual = Very_Easy.IsOdd(num);
+
+            // Asert
+            Assert.Equal(expected, actual);
+        }
+        
+        [Theory]
+        [InlineData("AB", "CD", true)]
+        [InlineData("ABC", "DE", false)]
+        [InlineData("hello", "edabit", false)]
+        [InlineData("meow", "woof", true)]
+        [InlineData("jrnvjrnnt", "cvjknfjvmfvnfjn", false)]
+        [InlineData("jkvnjrt", "krnf", false)]
+        [InlineData("Facebook", "Snapchat", true)]
+        public void Comp_ReturnTrueIfLengthsMatch (string str1, string str2, bool expected)
+        {
+            // Arrange
+
+            // Act
+            bool actual = Very_Easy.Comp(str1, str2);
+
+            // Asert
+            Assert.Equal(expected, actual);
+        }
+        
+        
+        [Theory]
         [InlineData(-4, -4)]
         [InlineData(212, -212)]
         [InlineData(-125, -125)]
@@ -22,7 +80,7 @@ namespace Edabit.Tests
             // Act
             int actual = Very_Easy.ReturnNegative(n);
 
-            // Asseert
+            // Assert
             Assert.Equal(expected, actual);
         }
         
