a3aec95fb45c65d05c1277dd736438b8f99667d7
mister-josuecedeno <josuecedeno@yahoo.com>
2023-12-11 07:59:51 -0600

    Circle: set-up

---
+++ Edabit/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit
+{
+    public class Rectangle
+    {
+        // https://edabit.com/challenge/kpReAapuDjgX2b4em
+
+        public double SideA;
+        public double SideB;
+
+        public Rectangle(double sideA, double sideB)
+        {
+            SideA = sideA;
+            SideB = sideB;
+        }
+
+        public double GetArea()
+        {
+            return SideA * SideB;
+        }
+
+        public double GetPerimeter()
+        {
+            return (SideA + SideB) * 2;
+        }
+    }
+}
