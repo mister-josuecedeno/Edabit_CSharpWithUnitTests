f265afe26f203950183661b82944ecb687268a99
mister-josuecedeno <josuecedeno@yahoo.com>
2023-12-13 20:39:43 -0600

    Circle: finished

---
+++ Edabit/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit
+{
+    class Circle
+    {
+        public double Radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public double GetPerimeter()
+        {
+            return Math.PI * Radius * 2;
+        }
+    }
+}
