da58a6cbdf9fd0588782c63a367bdad8a7c52493
mister-josuecedeno <josuecedeno@yahoo.com>
2024-02-18 08:57:49 -0600

    Set-up new folders (In Progress)

---
+++ Edabit/Easy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit
+{
+    public static class Easy
+    {
+        // https://edabit.com/challenge/xvSXDZh2GHE6F28SA
+        public static bool IsValid(string zip)
+        {
+            return false;
+        }
+    }
+}
