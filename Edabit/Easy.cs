7df10362f174dcfbaa2a8eac3de7bc0da794e037
mister-josuecedeno <josuecedeno@yahoo.com>
2024-02-23 07:20:54 -0600

    UniqueSort: set-up

--- Edabit/Easy.cs
+++ Edabit/Easy.cs
@@ -9,6 +9,12 @@ namespace Edabit
 {
     public static class Easy
     {
+        // https://edabit.com/challenge/9Emx6unF8evWiXnJA
+        public static double[] UniqueSort(double[] arr)
+        {
+            return new double[0];
+        }
+
         // https://edabit.com/challenge/xvSXDZh2GHE6F28SA
         public static bool IsValid(string zip)
         {
--- Edabit_Very_Easy_Test/Easy_Test.cs
+++ Edabit_Very_Easy_Test/Easy_Test.cs
@@ -6,6 +6,27 @@ namespace Edabit.Tests
     public class Easy_Test
     {
         [Theory]
+        [InlineData(
+            new double[] { 1, 2, 3, 4, 5, 8, 10 },
+            new double[] { 1, 5, 8, 2, 3, 4, 4, 4, 10 }
+        )]
+        [InlineData(new double[] { 1, 2, 4, 5, 7 }, new double[] { 1, 2, 5, 4, 7, 7, 7 })]
+        [InlineData(
+            new double[] { 0, 1, 2, 3, 4, 5, 6, 7 },
+            new double[] { 7, 6, 5, 4, 3, 2, 1, 0, 1 }
+        )]
+        public void ReturnUniqueSort(double[] arr, double[] expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = Easy.UniqueSort(arr);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
         [InlineData("59001", true)]
         [InlineData("853a7", false)]
         [InlineData("732 32", false)]
@@ -18,7 +39,7 @@ namespace Edabit.Tests
         {
             // Arrange
 
-            // Actual
+            // Act
             var actual = Easy.IsValid(zip);
 
             // Assert
