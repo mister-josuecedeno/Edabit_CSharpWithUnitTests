623befb4e92b2d4492470bfb9927a8cfa0da21b1
Josue Cedeno <josuecedeno@yahoo.com>
2024-11-04 07:33:11 -0600

    KToK: set-up

---
+++ Edabit/Medium.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edabit
+{
+    public static class Medium
+    {
+        // https://edabit.com/challenge/jxQTbif5fx7J8FeTT
+        public static bool KToK(string n, int k)
+        {
+            return false;
+        }
+
+    }
+}
---
+++ Edabit_Very_Easy_Test/Medium_Test.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.ComponentModel.DataAnnotations;
+
+namespace Edabit.Tests
+{
+    public class Medium_Test
+    {
+        [Theory]
+        [InlineData("4", 2, true)]
+        [InlineData("387420489", 9, true)]
+        [InlineData("302875106592253", 13, true)]
+        [InlineData("341427877364219557396646723584", 22, true)]
+        [InlineData("1330877630632711998713399240963346255985889330161650994325137953641", 41, true)]
+        public void ReturnKToK(string n, int k, bool expected)
+        {
+            // Arrange
+
+            // Actual
+            var actual = Medium.KToK(n, k);
+            
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
