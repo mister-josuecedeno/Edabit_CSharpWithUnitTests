1cdf3ed6c2f99c31f5df67abcee177a3550762af
Josue Cedeno <josuecedeno@yahoo.com>
2025-02-16 08:49:41 -0600

    IsPrime: finished

--- Edabit/Medium.cs
+++ Edabit/Medium.cs
@@ -19,7 +19,27 @@ namespace Edabit
         // https://edabit.com/challenge/zHXnh6Eoej7NrHvFu
         public static string IsPrime(int[] primes, int num)
         {
-            return string.Empty;
+            int low = 0, high = primes.Length - 1;
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+
+                // Check if x is present at mid
+                if (primes[mid] == num)
+                    return "yes";
+
+                // If x greater, ignore left half
+                if (primes[mid] < num)
+                    low = mid + 1;
+
+                // If x is smaller, ignore right half
+                else
+                    high = mid - 1;
+            }
+
+            // If we reach here, then element was
+            // not present
+            return "no";
         }
 
         // https://edabit.com/challenge/AafHF2YMGCqDRWERo
